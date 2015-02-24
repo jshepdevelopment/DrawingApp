92a55a2cdded0a8857559f60d5a7678817cab21c
Jason Shepherd <jshepherd@outlook.com>
2015-02-23 22:05:01 -0700

    Completed paint and color selection.

--- app/src/main/java/com/rickgoldman/drawingapp/MainActivity.java
+++ app/src/main/java/com/rickgoldman/drawingapp/MainActivity.java
@@ -4,14 +4,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private DrawingView drawView;
+    private ImageButton currPaint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawView = (DrawingView)findViewById(R.id.drawing);
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
     }
 
 
@@ -36,4 +46,19 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void paintClicked(View view){
+        //use chosen color
+        if(view!=currPaint){
+        //update color
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint=(ImageButton)view;
+        }
+
+    }
+
 }
